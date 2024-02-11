cc20931d54e4e20ae44e1da90cd5a5267cb58c05
Tomoya Kose <tomoya@mitsuse.jp>
2024-02-11 21:03:15 +0900

    Fix the min version of iOS.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Blueprint",
+    platforms: [.iOS(.v14)],
     products: [
         .library(name: "Blueprint", targets: ["Blueprint"]),
     ],
